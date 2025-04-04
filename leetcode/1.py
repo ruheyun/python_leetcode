3724cfe08b059cbbae945d814ebce89471a3fbe4
ruheyun <1654033661@qq.com>
2025-04-04 17:24:03 +0800

    modified leetcode/1.py

--- leetcode/1.py
+++ leetcode/1.py
@@ -5,14 +5,31 @@ from typing import List
 
 
 class Solution:
+    """
+    时间复杂度：n^2
+    空间复杂度：1
+    """
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums) - 1):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
+class Solution2:
+    """
+    时间复杂度：n
+    空间复杂度: n
+    """
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        idx = {}
+        for j, x in enumerate(nums):
+            if target - x in idx:
+                return [idx[target - x], j]
+            idx[x] = j
+
 
 if __name__ == '__main__':
-    solution = Solution()
+    solution = Solution2()
     list = solution.twoSum([2, 4, 1, 7, 0], 5)
+
     print(list)
