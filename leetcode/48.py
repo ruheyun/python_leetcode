a73a2f9e7445dc1fae64bf728d29adfbe91b0fd7
ruheyun <1654033661@qq.com>
2025-04-16 21:54:21 +0800

    update 48.py leetcode

--- leetcode/48.py
+++ leetcode/48.py
@@ -13,4 +13,15 @@ def rotate(matrix: List[List[int]]) -> None:
     print(matrix)
 
 
+def rotate2(matrix: List[List[int]]) -> None:
+    n = len(matrix)
+    for i in range(n // 2):
+        for j in range((n + 1) // 2):
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[n - 1 - j][i]
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+            matrix[j][n - 1 - i] = tmp
+
+
 rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]])
