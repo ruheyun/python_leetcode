2b20381e4ce314aaa4f05b67221039b88dc30f77
ruheyun <1654033661@qq.com>
2025-04-17 19:07:58 +0800

    add 240.py

---
+++ leetcode/240.py
@@ -0,0 +1,23 @@
+# coding:UTF-8
+# RuHe  2025/4/17 19:01
+# 搜索二维矩阵
+from typing import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        i, j = len(matrix) - 1, 0
+        while i >= 0 and j < len(matrix[0]):
+            if matrix[i][j] > target:
+                i -= 1
+            elif matrix[i][j] < target:
+                j += 1
+            else:
+                return True
+        return False
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    rst = solution.searchMatrix([[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], 31)
+    print(rst)
